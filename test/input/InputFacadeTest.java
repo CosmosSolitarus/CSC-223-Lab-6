748e2d53382ed63df91ac5d64a3ed52697658419
CosmosSolitarus <156846497+CosmosSolitarus@users.noreply.github.com>
2024-03-26 14:13:58 -0400

    InputFacadeTest broken

--- test/input/InputFacadeTest.java
+++ test/input/InputFacadeTest.java
@@ -2,7 +2,21 @@ package input;
 
 import org.junit.Test;
 
+import geometry_objects.Segment;
+import geometry_objects.points.PointDatabase;
+import input.builder.GeometryBuilder;
+import input.components.ComponentNode;
+import input.components.FigureNode;
+import input.parser.JSONParser;
+import input.visitor.UnparseVisitor;
+
 import static org.junit.Assert.*;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.json.JSONObject;
 
 public class InputFacadeTest {
     @Test
@@ -12,6 +26,15 @@ public class InputFacadeTest {
 
     @Test
     public void toGeometryRepresentationTest() {
-        
+        FigureNode fig = InputFacade.extractFigure("single_triangle.json");
+
+
+        Map.Entry<PointDatabase, Set<Segment>> map = InputFacade.toGeometryRepresentation(fig);
+
+        PointDatabase pdb = map.getKey();
+        Set<Segment> segments = map.getValue();
+
+        assertEquals(3, pdb.size());
+        assertEquals(3, segments.size());
     }
 }
