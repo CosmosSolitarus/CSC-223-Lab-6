7edb6d17591e9b218ab1c20f10de11a7835bfe13
CosmosSolitarus <156846497+CosmosSolitarus@users.noreply.github.com>
2024-03-26 14:33:44 -0400

    PointNamingFactoryTest complete and all tests passed

--- test/geometry_objects/points/PointNamingFactoryTest.java
+++ test/geometry_objects/points/PointNamingFactoryTest.java
@@ -2,6 +2,9 @@ package geometry_objects.points;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import org.junit.Test;
 
 public class PointNamingFactoryTest {
@@ -29,41 +32,183 @@ public class PointNamingFactoryTest {
     
     @Test
     public void putPTTest() {
+        PointNamingFactory pnf = new PointNamingFactory();
 
+        Point pt0 = new Point("bruh", -2, 0.337);
+        Point pt1 = new Point("bro", -2, 0.337);
+
+        assertEquals(0, pnf.size());
+
+        assertEquals(pt0, pnf.put(pt0));
+        assertEquals(1, pnf.size());
+
+        assertEquals(pt0, pnf.put(pt0));
+        assertEquals(1, pnf.size());
+
+        assertEquals(pt0, pnf.put(pt1));
+        assertEquals(1, pnf.size());
+
+        assertEquals(new Point(-1, 2), pnf.put(new Point(-1, 2)));
+        assertEquals(2, pnf.size());
     }
 
     @Test
     public void putXYest() {
-        
+        PointNamingFactory pnf = new PointNamingFactory();
+
+        Point pt0 = new Point("bruh", -2, 0.337);
+        Point pt1 = new Point("bro", -2, 0.337);
+
+        assertEquals(0, pnf.size());
+
+        assertEquals(pt0, pnf.put(-2, 0.337));
+        assertEquals(1, pnf.size());
+
+        assertEquals(pt0, pnf.put(-2, 0.337));
+        assertEquals(1, pnf.size());
+
+        assertEquals(pt0, pnf.put(-2, 0.337));
+        assertEquals(1, pnf.size());
+
+        assertEquals(new Point(-1, 2), pnf.put(new Point(-1, 2)));
+        assertEquals(2, pnf.size());
     }
 
     @Test
     public void putTest() {
-        
+        PointNamingFactory pnf = new PointNamingFactory();
+
+        Point pt0 = new Point("bruh", -2, 0.337);
+
+        assertEquals(0, pnf.size());
+
+        assertEquals(pt0, pnf.put("", -2, 0.337));
+        assertEquals(1, pnf.size());
+
+        assertEquals(pt0, pnf.put("bruh", -2, 0.337));
+        assertEquals(1, pnf.size());
+
+        assertEquals(pt0, pnf.put("bruh", -2, 0.337));
+        assertEquals(1, pnf.size());
+
+        assertEquals(pt0, pnf.put("bro", -2, 0.337));
+        assertEquals(1, pnf.size());
+
+        assertEquals(new Point(-1, 2), pnf.put(new Point("hey", -1, 2)));
+        assertEquals(2, pnf.size());
     }
 
     @Test
     public void getXYTest() {
-        
+        PointNamingFactory pnf = new PointNamingFactory();
+
+        Point pt0 = new Point("bruh", -2, 0.337);
+
+        pnf.put(pt0);
+
+        assertEquals(pt0, pnf.get(-2, 0.337));
+
     }
 
     @Test
     public void getPTTest() {
-        
+        PointNamingFactory pnf = new PointNamingFactory();
+
+        Point pt0 = new Point("bruh", -2, 0.337);
+        Point pt1 = new Point("yo-yo", 11, 97);
+        Point pt2 = new Point("John", 33, 37);
+        Point pt3 = new Point("CS>MATH", 0.6, -4);
+
+        Set<Point> pts = new LinkedHashSet<Point>();
+
+        pnf.put(pt0);
+        pnf.put(pt1);
+        pnf.put(pt2);
+        pnf.put(pt3);
+
+        pts.add(pt0);
+        pts.add(pt1);
+        pts.add(pt2);
+        pts.add(pt3);
+
+        for (Point pt : pts) {
+            assertEquals(pt, pnf.get(pt));
+        }
     }
 
     @Test
     public void containsXYTest() {
-        
+        PointNamingFactory pnf = new PointNamingFactory();
+
+        Point pt0 = new Point("bruh", -2, 0.337);
+        Point pt1 = new Point("yo-yo", 11, 97);
+        Point pt2 = new Point("John", 33, 37);
+        Point pt3 = new Point("CS>MATH", 0.6, -4);
+
+        Set<Point> pts = new LinkedHashSet<Point>();
+
+        pnf.put(pt0);
+        pnf.put(pt1);
+        pnf.put(pt2);
+        pnf.put(pt3);
+
+        pts.add(pt0);
+        pts.add(pt1);
+        pts.add(pt2);
+        pts.add(pt3);
+
+        for (Point pt : pts) {
+            assertTrue(pnf.contains(pt.getX(), pt.getY()));
+        }
     }
 
     @Test
     public void containsPTTest() {
-        
+        PointNamingFactory pnf = new PointNamingFactory();
+
+        Point pt0 = new Point("bruh", -2, 0.337);
+        Point pt1 = new Point("yo-yo", 11, 97);
+        Point pt2 = new Point("John", 33, 37);
+        Point pt3 = new Point("CS>MATH", 0.6, -4);
+
+        Set<Point> pts = new LinkedHashSet<Point>();
+
+        pnf.put(pt0);
+        pnf.put(pt1);
+        pnf.put(pt2);
+        pnf.put(pt3);
+
+        pts.add(pt0);
+        pts.add(pt1);
+        pts.add(pt2);
+        pts.add(pt3);
+
+        for (Point pt : pts) {
+            assertTrue(pnf.contains(pt));
+        }
     }
 
     @Test
     public void getAllPoints() {
-        
+        PointNamingFactory pnf = new PointNamingFactory();
+
+        Point pt0 = new Point("bruh", -2, 0.337);
+        Point pt1 = new Point("yo-yo", 11, 97);
+        Point pt2 = new Point("John", 33, 37);
+        Point pt3 = new Point("CS>MATH", 0.6, -4);
+
+        Set<Point> pts = new LinkedHashSet<Point>();
+
+        pnf.put(pt0);
+        pnf.put(pt1);
+        pnf.put(pt2);
+        pnf.put(pt3);
+
+        pts.add(pt0);
+        pts.add(pt1);
+        pts.add(pt2);
+        pts.add(pt3);
+
+        assertEquals(pts, pnf.getAllPoints());
     }
 }
